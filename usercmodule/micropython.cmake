84b54ed69649fb509c4252a362f3ff9e7a830dcf
Katsuhiro Mihara <katsuhiro.mihara@access-company.com>
2021-07-30 14:57:59 +0900

    [229140] - Remove C module source code.

--- usercmodule/micropython.cmake
+++
@@ -1,7 +0,0 @@
-# This top-level micropython.cmake is responsible for listing
-# the individual modules we want to include.
-# Paths are absolute, and ${CMAKE_CURRENT_LIST_DIR} can be
-# used to prefix subdirectories.
-
-# Add the POWERGs module.
-include(${CMAKE_CURRENT_LIST_DIR}/powergs/micropython.cmake)
